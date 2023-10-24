785ad7785e5847f2829228a66b39997fbad610ba
SYNDAYMGP <142337606+SYNDAYMGP@users.noreply.github.com>
2023-10-24 20:40:44 +0300

    Update боты версии.py
    3

--- боты версии.py
+++ боты версии.py
@@ -18,7 +18,20 @@ bot.polling(none_stop=True, interval=0)
 @@@@
 @@@@
 @@@@
-///
+///принемает текстовое сообщение пользователя, сохраняет его в txt файл при помощи раскадировки utf-8, 
+отправляет текстовое сообщение пользователю обратно.
+import telebot
+bot = telebot.TeleBot('6786106072:AAGz8bE4mNyJscUDrvARjYiXOCAyX5FukD4')
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, 'Я на связи. Напишите мне что-нибудь)')
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    text = message.text
+    with open('1.txt', 'a', encoding='utf-8') as file:
+        file.write(text + '\n')
+    bot.send_message(message.chat.id, 'Вы написали: ' + text)
+bot.polling(none_stop=True, interval=0)
 
 
 
