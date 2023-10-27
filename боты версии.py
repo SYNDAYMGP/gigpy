fb7d04fa57c97c2be6b4bca1eb03f3f7d6b226ce
SYNDAYMGP <142337606+SYNDAYMGP@users.noreply.github.com>
2023-10-27 11:21:29 +0300

    Update боты версии.py
    1

--- боты версии.py
+++ боты версии.py
@@ -55,7 +55,32 @@ def handle_text(message):
     with open('1.txt', 'rb') as file:
         bot.send_document(message.chat.id, file)
 bot.polling(none_stop=True, interval=0)
-
+///
+@@@@
+@@@@
+@@@@
+@@@@
+@@@@
+///принемает текстовое сообщение пользователя, обрабатывает его через gp и ответ gp отсылает 
+обратно пользователю в виде txt файла.
+import g4f
+import telebot
+bot = telebot.TeleBot('6786106072:AAGz8bE4mNyJscUDrvARjYiXOCAyX5FukD4')
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+  with open('1.txt', 'w') as file:
+    file.write('')
+  text = message.text
+  response = g4f.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": text}], 
+    stream=True
+  )
+  for msg in response:
+    with open('1.txt', 'a', encoding='utf-8') as file:
+      file.write(msg + '\n')
+  bot.send_document(message.chat.id, open('1.txt', 'rb'))
+bot.polling(none_stop=True, interval=0)
 
 
 
