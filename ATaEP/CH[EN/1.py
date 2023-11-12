8b94d5847d8b89445a9e49f51fbf6578c71d9844
SYNDAYMGP <142337606+SYNDAYMGP@users.noreply.github.com>
2023-11-12 22:15:31 +0300

    Update 1.py
    1

--- ATaEP/CH[EN/1.py
+++ ATaEP/CH[EN/1.py
@@ -11,3 +11,13 @@ filled_text = '\n'.join(filled_lines)
 with open(output_file_path, 'w', encoding='utf-8') as output_file:
     output_file.write(filled_text)
 print(f'Заполненный текст сохранен в файле: {output_file_path}')
+
+#сохранение глав по очереди
+import re
+with open('1.txt', 'r', encoding='utf-8') as file:
+  lines = file.readlines()
+ww_vars = []
+for line in lines:
+  if re.search(r'\d', line) and len(line.strip()) > 0:
+    ww_vars.append(line)
+print(ww_vars[0])
